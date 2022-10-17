9550ec30039d00b8a244cb5be4eb5b2809cace85
Majoross Dániel <majoross.d@gmail.com>
2022-10-17 12:43:00 +0200

    coctails populated from api and styled

--- src/components/cocktail-card/cocktail-card.component.tsx
+++ src/components/cocktail-card/cocktail-card.component.tsx
@@ -10,7 +10,9 @@ const CocktailCard = ({ imgUrl, description, title }: Iprops) => {
   return (
     <>
       <div className="card">
-        <img className="featured-image" src={imgUrl}></img>
+        <div className="img-wrapper">
+          <img className="featured-image" src={imgUrl}></img>
+        </div>
         <div className="content-wrapper">
           <h2 className="title">{title}</h2>
           <div className="description">{description}</div>
